8d92391aace740859ff96d61617e86ced165f607
Dem757 <demenybalint@gmail.com>
2023-04-29 16:35:59 +0200

    Kinda working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,109 @@
+import java.util.*;
+
 public class Main {
+    public static void countChars(char[] arr) {
+        int c = 0;
+        for (char value : arr) {
+            if (value != '-')
+                c++;
+        }
+        System.out.println(c);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+
+            // Ha az EOF jelet olvassuk, kilépünk a ciklusból
+            if (line.equals("EOF")) {
+                break;
+            }
+
+            // Üres sorokat figyelmen kívül hagyjuk
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            String[] parts = line.split(",");
+
+            for (String part : parts) {
+                int number = Integer.parseInt(part.trim());
+
+                // Ha a szám negatív, akkor a pozitív változatát tároljuk el
+                if (number < 0) {
+                    number = -number;
+                }
+
+                numbers.add(number);
+            }
+        }
+        LinkedList<Page> fifo = new LinkedList<>();
+        boolean next = false;
+        char[] out = new char[numbers.size()];
+
+        Page A = new Page('A');
+        Page B = new Page('B');
+        Page C = new Page('C');
+
+
+        A.setNumber(numbers.get(0));
+        fifo.add(A);
+        out[0] = 'A';
+
+        for (int i = 1; i < numbers.size(); ++i) {
+            // Végig megyünk a fifon, ha találunk benne egy olyan lapot amelyiknek megegyezik a száma akkor kilépünk.
+            for(int j = 0; j < fifo.size(); ++j) {
+                if(numbers.get(i) == fifo.get(j).getNumber()) {
+                    out[i] = '-';
+                    fifo.get(j).setFreezeTime(0);
+                    fifo.get(j).setSc(true);
+                    next = true;
+                }
+            }
+            if (!next) {
+                if(fifo.size() < 3) {
+                    if(fifo.size() == 1) {
+                        B.setNumber(numbers.get(i));
+                        fifo.add(B);
+                        out[i] = 'B';
+                    }
+                    else if(fifo.size() == 2) {
+                        C.setNumber(numbers.get(i));
+                        fifo.add(C);
+                        out[i] = 'C';
+                    }
+                }else {
+                    int c = 0;
+                    while(c < 3) {
+                        Page current = fifo.removeFirst();
+                        if(!current.isSc() && !current.isFrozen()) {
+                            current.setNumber(numbers.get(i));
+                            current.setFrozen(true);
+                            current.setSc(false);
+                            current.setFreezeTime(4);
+                            fifo.add(current);
+                            out[i] = current.getCh();
+                            break;
+                        } else {
+                            fifo.add(current);
+                        }
+                        c++;
+                    }
+                    if(c == 3) {
+                        out[i] = '*';
+                    }
+                }
+            }
+            for (Page page : fifo) {
+                page.decreaseF();
+            }
+
+            next = false;
+        }
+        System.out.println(out);
+        countChars(out);
     }
 }
---
+++ src/Page.java
@@ -0,0 +1,59 @@
+public class Page {
+    private int number;
+    private char ch;
+    private int freezeTime;
+    private boolean sc;
+    private boolean frozen;
+
+    public Page(char c) {
+        number = 0;
+        ch = c;
+        freezeTime = 3;
+        sc = false;
+        frozen = true;
+    }
+
+    public void setNumber(int n) {
+        number = n;
+    }
+
+    public void setFreezeTime(int f) {
+        freezeTime = f;
+    }
+
+    public void setSc(boolean s) {
+        sc = s;
+    }
+
+    public boolean isSc() {
+        return sc;
+    }
+
+    public char getCh() {
+        return ch;
+    }
+
+    public int getFreezeTime() {
+        return freezeTime;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
+
+    public void setFrozen(boolean frozen) {
+        this.frozen = frozen;
+    }
+
+    public void decreaseF() {
+        if(freezeTime > 0)
+            freezeTime--;
+        if(freezeTime == 0)
+            frozen = false;
+    }
+
+}
